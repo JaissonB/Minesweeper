5b6294e76914649901807be8eba9cb2e8d05751c
JaissonB <097319@aluno.uricer.edu.br>
2022-02-15 22:16:24 -0300

    Adição da função spreadNumbers

--- script.js
+++ script.js
@@ -1,24 +1,80 @@
-var lado = 3
-var size = lado * lado
+var lado = 9; //9(81-10) - 13(169-25) - 21(441-80)
+var size = lado * lado;
 
-var matriz = []
-var vetores = []
+var nivel = 1;
 
-for (var i = 0; i < size; i++) {
-    vetores.push(i)
-    if (vetores.length == lado) {
-        var divisor = (i + 1) / lado - 1;
-        matriz[divisor] = vetores
-        vetores = []
+var matriz = [];
+var vectors = [];
+
+//Muda a quantidade de bombas conforme o tamanho da matriz
+function defQuantityOfBombs () {
+    switch (lado) {
+        case 9:
+            return 10
+        case 13:
+            return 25
+        case 21:
+            return 80
     }
 }
 
-matriz[getRandomInt()][getRandomInt()] = 'X'
-console.log(matriz)
+//Inicia a matriz do tamanho necessário
+function initMatriz () {
+    matriz = [];
+    for (var i = 0; i < size; i++) {
+        vectors.push(0);
+        if (vectors.length == lado) {
+            var divisor = (i + 1) / lado - 1;
+            matriz[divisor] = vectors;
+            vectors = [];
+        }
+    }
+}
+initMatriz();
 
+//Retorna um numero pseudo-aleatorio para usar na distribuição das bombas
 function getRandomInt () {
     min = Math.ceil(0);
     max = Math.floor(lado);
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
+//Espalha aleatóriamente as minas pela matriz
+function spreadBombs () {
+    for (var i = 0; i < defQuantityOfBombs(); i ++) {
+        var linha = getRandomInt();
+        var coluna = getRandomInt();
+        if (matriz[linha][coluna] !== 'X') {
+            matriz[linha][coluna] = 'X';
+        } else i--;
+    }
+}
+spreadBombs();
+console.log(matriz)
+
+function spreadNumbers () {
+    //Percorre as linhas da matriz
+    for (var i = 0; i < lado; i ++) {
+        //Percorre as colunas da matriz
+        for (var j = 0; j < lado; j ++) {
+            console.log(i, j);
+            var quantityOfBombsAround = 0;
+            //Se tiver uma bomba não faz nada, segue para o próximo laço
+            if (matriz[i][j] !== 'X') {
+                //Não sendo bomba, será percorrido os arredores da casa em questão para indentificar quantas minas existem ao seu redor
+                for (var a = i - 1; a <= i + 1; a ++){
+                    for ( var b = j - 1; b <= j + 1; b ++) {
+                        if (a >= 0 && b >= 0 && a < lado && b < lado) {
+                            if (matriz[a][b] === 'X') {
+                                quantityOfBombsAround ++;
+                            }
+                        }
+                    }
+                }
+                matriz[i][j] = quantityOfBombsAround;
+            }
+        }
+    }
+    console.log("Spread:", matriz)
+}
+spreadNumbers();
